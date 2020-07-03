7839ea8dc52ca4078e3e725a277876aa782cae0e
evgenii.krivenko <jakos.soll@gmail.com>
2020-07-03 10:14:10 +0300

    Solve task with another method

--- checkio_tasks/checkio.py
+++ checkio_tasks/checkio.py
@@ -5,16 +5,17 @@ import collections
 
 
 def checkio(data: list) -> list:
-    c = collections.Counter(data)
-    # c.most_common()[:1:-1][0][0]
-    return data.remove()
+    non_unique = []
+    for i in data:
+        if data.count(i) != 1:
+            non_unique.append(i)
+    return non_unique
 
 
 if __name__ == "__main__":
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-    print(checkio([1, 2, 3, 1, 3]))
-    # assert list(checkio([1, 2, 3, 1, 3])) == [1, 3, 1, 3], "1st example"
-    # assert list(checkio([1, 2, 3, 4, 5])) == [], "2nd example"
-    # assert list(checkio([5, 5, 5, 5, 5])) == [5, 5, 5, 5, 5], "3rd example"
-    # assert list(checkio([10, 9, 10, 10, 9, 8])) == [10, 9, 10, 10, 9], "4th example"
-    # print("It is all good. Let's check it now")
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert list(checkio([1, 2, 3, 1, 3])) == [1, 3, 1, 3], "1st example"
+    assert list(checkio([1, 2, 3, 4, 5])) == [], "2nd example"
+    assert list(checkio([5, 5, 5, 5, 5])) == [5, 5, 5, 5, 5], "3rd example"
+    assert list(checkio([10, 9, 10, 10, 9, 8])) == [10, 9, 10, 10, 9], "4th example"
+    print("It is all good")
